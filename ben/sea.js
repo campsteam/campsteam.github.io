2ca26a4818ccc193c3d98ae60e6be0a843ece1bf
Camp <camp@c02kw1bzfh51.dalton.org>
2016-06-24 11:41:08 -0400

    updated ben's site

--- ben/sea.js
+++ ben/sea.js
@@ -5,4 +5,10 @@ $(document).ready(function() {
              window.location.href ="https://www.google.com/search?q="+$("#search").val();
 	   }
          });
+	$("#cookie").keypress(function(e){
+	   if (e.keyCode === 13) {
+	   
+             window.location.href ="https://www.google.com/search?q="+$("#cookie").val()+"&espv=2&biw=1366&bih=696&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjFp-je_cDNAhVFeSYKHRiVDu8Q_AUIBigB";
+	   }
+         });
 });
